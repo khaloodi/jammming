de3ba06c17538849a195750c00d0a3aa8136e9c7
Khaled Adad <khaledadad@lewisu.edu>
2020-05-11 12:21:27 -0500

    wip

--- jammmming_app/src/components/App/App.js
+++ jammmming_app/src/components/App/App.js
@@ -6,7 +6,7 @@ import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1},
@@ -17,7 +17,27 @@ class App extends React.Component {
         { name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5 },
         { name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6 }]
     }
+    this.addTrack = this.addTrack.bind(this)
+    this.removeTrack = this.removeTrack.bind(this)
   }
+
+  addTrack =(track)=> {
+    let tracks = this.state.playlistTracks
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return 
+    }
+
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks })
+  }
+
+  removeTrack =(track)=> {
+    let tracks = this.state.playlistTracks
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id)
+
+    this.setState({ playlistTracks: tracks })
+  }
+
   render() {
     return (
       <div>
@@ -25,8 +45,8 @@ class App extends React.Component {
           <div class="App">
             <SearchBar />
             <div className="App-playlist">
-              <SearchResults searchResults={this.state.searchResults}/>              
-              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+              <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>              
+              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
             </div>
           </div>
       </div>
