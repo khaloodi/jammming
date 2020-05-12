062461a6605eb6ba8f44c3f8c140dd7a19a1449b
Khaled Adad <khaledadad@lewisu.edu>
2020-05-12 12:58:35 -0500

    save to spotify and update playlist done

--- jammmming_app/src/components/App/App.js
+++ jammmming_app/src/components/App/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this)
     this.removeTrack = this.removeTrack.bind(this)
     this.updatePlaylistName = this.updatePlaylistName.bind(this)
+    this.savePlaylist = this.savePlaylist.bind(this)
   }
 
   addTrack =(track)=> {
@@ -43,6 +44,11 @@ class App extends React.Component {
     this.setState({ playlistName: name})
   }
 
+  savePlaylist() {
+    alert('this method is linked to button correctly')
+    const trackUris = this.state.playlistTracks.map(track => track.uri)
+  }
+
   render() {
     return (
       <div>
@@ -51,7 +57,8 @@ class App extends React.Component {
             <SearchBar />
             <div className="App-playlist">
               <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>              
-              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
+              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}/>
             </div>
           </div>
       </div>
