7da4f8b82173d4402b46d3065c42e673bc3b1b7f
Khaled Adad <khaledadad@lewisu.edu>
2020-05-12 12:16:06 -0500

    wip

--- jammmming_app/src/components/App/App.js
+++ jammmming_app/src/components/App/App.js
@@ -38,6 +38,10 @@ class App extends React.Component {
     this.setState({ playlistTracks: tracks })
   }
 
+  updatePlaylistName =(name)=> {
+    this.setState({ playlistName: name})
+  }
+
   render() {
     return (
       <div>
