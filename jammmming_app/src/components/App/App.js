7c007cdd262571e9272bd51fbf90acff449a523d
Khaled Adad <khaledadad@lewisu.edu>
2020-05-12 12:37:29 -0500

    wip

--- jammmming_app/src/components/App/App.js
+++ jammmming_app/src/components/App/App.js
@@ -19,6 +19,7 @@ class App extends React.Component {
     }
     this.addTrack = this.addTrack.bind(this)
     this.removeTrack = this.removeTrack.bind(this)
+    this.updatePlaylistName = this.updatePlaylistName.bind(this)
   }
 
   addTrack =(track)=> {
@@ -50,7 +51,7 @@ class App extends React.Component {
             <SearchBar />
             <div className="App-playlist">
               <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>              
-              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
+              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
             </div>
           </div>
       </div>
